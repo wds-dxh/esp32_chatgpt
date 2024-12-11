684af8d6db0546085dc2847ba908208e09d5c936
wds-Ubuntu22-cqu <wdsnpshy@163.com>
2024-12-11 12:10:38 +0800

    update_2024-12-11 12:10:38_吃饭

--- src/main.cpp
+++ src/main.cpp
@@ -5,79 +5,17 @@
  * 邮箱：wdsnpshy@163.com 
  * Copyright (c) 2024 by ${wds-Ubuntu22-cqu}, All Rights Reserved. 
  */
-// Welcome to www.LingShunLAB.com
 
 #include "Arduino.h"
-#include "WiFiMulti.h"
-#include "Audio.h"
-
-// Digital I/O used
-#define I2S_DOUT 25 // DIN引脚
-#define I2S_BCLK 26 // BCLK引脚
-#define I2S_LRC 27  // LRC引脚
 
 
-Audio audio;
-WiFiMulti wifiMulti;
-
-// config your wifi
-String ssid =     "wds";
-String password = "wds666666";
 
 void setup() {
 
-    Serial.begin(115200);
-
-    WiFi.mode(WIFI_STA);
-    wifiMulti.addAP(ssid.c_str(), password.c_str());
-    wifiMulti.run();
-    if(WiFi.status() != WL_CONNECTED){
-        WiFi.disconnect(true);
-        wifiMulti.run();
-    }
-    audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
-    audio.setVolume(80); // 0...21
-
-    // audio.connecttohost("http://mp3.ffh.de/radioffh/hqlivestream.mp3"); //  128k mp3
-    audio.connecttohost("https://m10.music.126.net/20241210222019/3c37feb8ad0ae3e5305dc5f9027c70eb/ymusic/5353/0f0f/0358/d99739615f8e5153d77042092f07fd77.mp3"); //  128k mp3
 }
 
 void loop()
 {
-    audio.loop();
-    if(Serial.available()){ // put streamURL in serial monitor
-        audio.stopSong();
-        String r=Serial.readString(); r.trim();
-        if(r.length()>5) audio.connecttohost(r.c_str());
-        log_i("free heap=%i", ESP.getFreeHeap());
-    }
+
 }
 
-// optional
-void audio_info(const char *info){
-    Serial.print("info        "); Serial.println(info);
-}
-void audio_id3data(const char *info){  //id3 metadata
-    Serial.print("id3data     ");Serial.println(info);
-}
-void audio_eof_mp3(const char *info){  //end of file
-    Serial.print("eof_mp3     ");Serial.println(info);
-}
-void audio_showstation(const char *info){
-    Serial.print("station     ");Serial.println(info);
-}
-void audio_showstreamtitle(const char *info){
-    Serial.print("streamtitle ");Serial.println(info);
-}
-void audio_bitrate(const char *info){
-    Serial.print("bitrate     ");Serial.println(info);
-}
-void audio_commercial(const char *info){  //duration in sec
-    Serial.print("commercial  ");Serial.println(info);
-}
-void audio_icyurl(const char *info){  //homepage
-    Serial.print("icyurl      ");Serial.println(info);
-}
-void audio_lasthost(const char *info){  //stream URL played
-    Serial.print("lasthost    ");Serial.println(info);
-}
